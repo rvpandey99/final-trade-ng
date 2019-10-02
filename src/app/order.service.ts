bd089451da905cb4f2eff03c7d902955de643288
Ravi Prakash Pandey <rvpandey99@gmail.com>
2019-10-03 00:47:41 +0530

    adding

---
+++ src/app/order.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+  constructor(private _http: HttpClient) { }
+  token = localStorage.getItem('token');
+
+  buy(body:any) {
+    return this._http.post('https://trade-ravi.herokuapp.com/buy',body,{headers:{"authToken":this.token}});
+  }
+
+  sell(body:any) {
+    return this._http.post('https://trade-ravi.herokuapp.com/sell',body,{headers:{"authToken":this.token}});
+  }
+
+  getStocks() {
+    return this._http.get('https://trade-ravi.herokuapp.com/stocks',{headers:{"authToken":this.token}});
+  }
+
+  getOrders() {
+    return this._http.get('https://trade-ravi.herokuapp.com/orders',{headers:{"authToken":this.token}});
+  }
+
+  getTrades() {
+    return this._http.get('https://trade-ravi.herokuapp.com/trades',{headers:{"authToken":this.token}});
+  }
+}
